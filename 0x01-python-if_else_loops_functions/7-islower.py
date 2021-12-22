794edf662cb01f9ffa48c9c8a713d2d826143181
3387-hub <3387@holbertonschool.com>
2021-12-21 21:45:15 -0500

    I write a function that checks for lowercase character

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+islower = __import__('7-islower').islower
+def islower(c):
+    for c in range(97,123):
+        if c is True:
+            return True
+        else:
+            return False
