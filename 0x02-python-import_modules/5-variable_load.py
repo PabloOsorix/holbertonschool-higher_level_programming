c7efd19f38da6c14c00ab6486df46b18619c19f5
3387-hub <3387@holbertonschool.com>
2021-12-28 17:25:47 -0500

    i write a program that imports the variable a from the file.

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 from variable_load_5 import a
 
+def print_var_value():
+    print(a)
 
 if __name__ == "__main__":
-    def print_var_value():
-        print(a)
+    print_var_value()
