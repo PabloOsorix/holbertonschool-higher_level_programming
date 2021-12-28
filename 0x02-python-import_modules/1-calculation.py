80c7b768cb4759a7c6a16182662ecc66e376e651
3387-hub <3387@holbertonschool.com>
2021-12-28 13:51:03 -0500

    I write a program that imports functions from the file calculator_1.py, does some Maths, and prints the result.

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    a = 10 
+    b = 5
+    result = add(a, b)
+    print("{:d} + {:d} = {result}")
+    result = sub(a, b)
+    print("{:d} + {:d} = {result}")
+    result = mul(a, b)
+    print("{:d} + {:d} = {result}")
+    result = div(a, b)
+    print("{:d} + {:d} = {result}")
