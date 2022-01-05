14395cee516b429458617b8c722f8eaba6fca652
3387-hub <3387@holbertonschool.com>
2022-01-05 14:08:07 -0500

    I write a function that replaces or adds key/value in a dictionary.

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -2,17 +2,16 @@
 
 
 def update_dictionary(a_dictionary, key, value):
-    new_dict = a_dictionary.copy()
     count = 0
 
     if key.isalpha():
-        for key_i, value_i in new_dict.items():
+        for key_i, value_i in a_dictionary.items():
             if key_i == key:
-                new_dict[key] = value
+                a_dictionary[key] = value
                 count += 1
 
         if count == 0:
             key = {key: value}
-            new_dict.update(key)
+            a_dictionary.update(key)
 
-        return new_dict
+        return a_dictionary
