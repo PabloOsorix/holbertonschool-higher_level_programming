36db36df3576299467d650ae893b9540200bf9e2
3387-hub <3387@holbertonschool.com>
2022-01-05 19:36:27 -0500

    I create a function def roman_to_int(roman_string): that converts a Roman numeral to an integer.

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -5,39 +5,15 @@ def roman_to_int(roman_string):
 
     if isinstance(roman_string, str) is False or roman_string is None:
         return(0)
-    roman_string = roman_string.upper()
-    roman_number = {"I": 1, "IV": 4, "V": 5, "IX": 9, "X": 10,
-                    "XL": 40, "L": 50, "XC": 90, "C": 100, "CD": 400,
-                    "D": 500, "CM": 900, "M": 1000}
-    counter = 0
-    counter_two = 0
-    for i in range(len(roman_string) - 1):
-        if i + 1 < len(roman_string):
-            counter_two = 0
-            if roman_string[i] == 'I' and roman_string[i + 1] == 'V':
-                counter += 4
-                counter_two += 1
-                continue
-            if roman_string[i] == 'I' and roman_string[i + 1] == 'X':
-                counter += 9
-                counter_two += 1
-                continue
-            if roman_string[i] == 'X' and roman_string[i + 1] == 'L':
-                counter += 40
-                counter_two += 1
-                continue
-            if roman_string[i] == 'X' and roman_string[i + 1] == 'C':
-                counter += 90
-                counter_two += 1
-                continue
-            if roman_string[i] == 'C' and roman_string[i + 1] == 'D':
-                counter += 400
-                counter_two += 1
-                continue
-            if roman_string[i] == 'C' and roman_string[i + 1] == 'M':
-                counter += 900
-                counter_two += 1
-                continue
-        if roman_number.get(roman_string[i]) is not None and counter_two == 0:
-            counter += roman_number.get(roman_string[i])
-    return counter
+    else:
+        num_ro = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500,
+                  'M': 100}
+        counter = 0
+        for i in range(len(roman_string) - 1):
+            if num_ro[roman_string[i]]\
+                    < num_ro[roman_string[i + 1]]:
+                counter = counter - num_ro[roman_string[i]]
+            else:
+                counter = counter + num_ro[roman_string[i]]
+        counter = counter + num_ro[roman_string[-1]]
+        return(counter)
