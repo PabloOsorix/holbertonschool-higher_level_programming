d6949eebcf6bfca5f7b25ac54d8f0a1dd0a53043
3387-hub <3387@holbertonschool.com>
2022-01-17 17:48:17 -0500

    :bug: fix : in .format

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,8 +4,8 @@ def safe_print_list(my_list=[], x=0):
     i = 0
     try:
         for i in my_list[:x]:
-            print("{:}".format(i), end="")
+            print("{}".format(i), end="")
         print()
     except:
-        return(i)
+        return i
     return i
