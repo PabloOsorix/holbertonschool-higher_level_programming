01761df201a9d398d8c5839f5fae1bac2f8226ad
3387-hub <3387@holbertonschool.com>
2022-01-17 18:33:47 -0500

    :bug: Delete the innecessary cast

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python3
 
 
-from typing import Type
-
-
 def safe_print_integer(value):
-
     try:
-        if int(value):
-            print("{:d}".format(value))
-            return True
-    except Type:
+        print("{:d}".format(value))
+        return True
+    except:
         return False
