6d99fa37d6df417edb61e50e1b208cef84c14934
3387-hub <3387@holbertonschool.com>
2022-01-17 20:31:16 -0500

    :bug: fix decimal float number.

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -8,4 +8,4 @@ def safe_print_division(a, b):
     except ZeroDivisionError:
         return None
     finally:
-        print("Inside result: {}".format(result))
+        print("Inside result: {:.1f}".format(result))
