039edf77f14e036f4e0813d350dc6f65a8ee2f63
3387-hub <3387@holbertonschool.com>
2022-01-17 21:06:51 -0500

    :recycle: update variable result
    git push

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,14 +2,22 @@
 
 
 def safe_print_division(a, b):
-    result = 0
+    result = None
     try:
         result = a / b
+        return result
     except ZeroDivisionError:
         return None
     finally:
-        if result != 0:
-            print("Inside result: {:.1f}".format(result))
-        else:
-            result = None
-            print("Inside result: {}".format(result))
+        print("Inside result: {}".format(result))
+
+
+a = 12
+b = 2
+result = safe_print_division(a, b)
+print("{:d} / {:d} = {}".format(a, b, result))
+
+a = 12
+b = 0
+result = safe_print_division(a, b)
+print("{:d} / {:d} = {}".format(a, b, result))
