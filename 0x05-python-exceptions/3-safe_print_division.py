be454bd851dddc00f95f5157d191f1d6a7b965cd
3387-hub <3387@holbertonschool.com>
2022-01-17 20:39:37 -0500

    :bug: fix the print None in the Inside result

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -4,8 +4,23 @@
 def safe_print_division(a, b):
     result = 0
     try:
-        result = a // b
+        result = a / b
     except ZeroDivisionError:
         return None
     finally:
-        print("Inside result: {:.1f}".format(result))
+        if result != 0:
+            print("Inside result: {:.1f}".format(result))
+        else:
+            result = None 
+            print("Inside result: {}".format(result))
+
+
+a = 12
+b = 2
+result = safe_print_division(a, b)
+print("{:d} / {:d} = {}".format(a, b, result))
+
+a = 12
+b = 0
+result = safe_print_division(a, b)
+print("{:d} / {:d} = {}".format(a, b, result))
