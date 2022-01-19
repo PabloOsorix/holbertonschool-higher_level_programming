913a4665072e66e87909ac472cccad5c5899f9be
3387-hub <3387@holbertonschool.com>
2022-01-18 19:44:03 -0500

    :bug: Fix the elif and else

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -7,12 +7,11 @@ class Square:
     """Class that defines a square with private instance attribute size"""
     """verify if size of area is of the allow type and value"""
     def __intit__(self, size=0):
-
         if type(size) is not int:
             print("size must be an integer", end="")
             raise TypeError
-        elif size < 0:
+        if size < 0:
             print("suze must be >= 0", end="")
             raise ValueError
-        else:
-            self.__size = size
+
+        self.__size = size
