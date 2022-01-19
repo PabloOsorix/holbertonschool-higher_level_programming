66b3d278ad2568ea51e7269acc97bc19e03157e5
3387-hub <3387@holbertonschool.com>
2022-01-18 22:09:46 -0500

    :heavy_plus_sign: I write a method that print # * size, size times

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+"""Empty class Square that defines a square"""
+
+
+class Square:
+    """Class taClass that defines a square Instantiation with optional size"""
+    """Type and value of size arae verificated"""
+
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        """property modify private instance size with value also.
+        Setter evaluate if it of int type.
+        """
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+        self._size = value
+
+    def area(self):
+        """Instanse that return total of the Square are,it's public."""
+        return self.size * self.size
+
+    def my_print(self):
+        """my_print print # * self.size the self.size times, if self.size
+        is 0 print a new line
+        """
+        for i in range(self.size):
+            if self.size == 0:
+                print()
+            else:
+                print("#" * self.size, end="")
+                print()
