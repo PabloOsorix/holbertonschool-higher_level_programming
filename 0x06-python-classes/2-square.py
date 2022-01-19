d2700d643a40dfc40b7e0034327a40d34abf600b
3387-hub <3387@holbertonschool.com>
2022-01-18 20:19:26 -0500

    :recycle: rewritten the code

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -4,14 +4,13 @@
 
 
 class Square:
-    """Class that defines a square with private instance attribute size"""
-    """verify if size of area is of the allow type and value"""
-    def __intit__(self, size=0):
+    """Class that defines a square Instantiation with optional size"""
+    """Type and value of size arae verificated"""
+    def __init__(self, size=0):
         if type(size) is not int:
-            print("size must be an integer", end="")
-            raise TypeError
+                print("size must be an integer", end="")
+                raise TypeError
         if size < 0:
-            print("size must be >= 0", end="")
-            raise ValueError
-
+                print("size must be >= 0", end="")
+                raise ValueError
         self.__size = size
