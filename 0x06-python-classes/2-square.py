26abe986c6ca97492189149269d4ba6e91b421f0
3387-hub <3387@holbertonschool.com>
2022-01-18 19:39:48 -0500

    :heavy_plus_sign: I write a class Square that defines a square by: (based on 1-square.py)

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""Empty class that defines a square"""
+
+
+class Square:
+    """Class that defines a square with private instance attribute size"""
+    """verify if size of area is of the allow type and value"""
+    def __intit__(self, size=0):
+
+        if type(size) is not int:
+            print("size must be an integer", end="")
+            raise TypeError
+        elif size < 0:
+            print("suze must be >= 0", end="")
+            raise ValueError
+        else:
+            self.__size = size
