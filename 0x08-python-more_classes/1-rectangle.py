264a3ad4624415b70d5e4a95daee89209cff4257
PabloOsorix <3387@holbertonschool.com>
2022-01-24 20:33:23 -0500

    :recycle: Change if in line 20 and 35

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,35 +6,35 @@
 class Rectangle:
     """Rectangle class with atributes width and height"""
     def __init__(self, width=0, height=0):
-        self.height = height
         self.width = width
+        self.height = height
 
     @property
     def width(self):
         """property that return the width instance modify
         by the setter"""
-        return self._width
+        return self.__width
 
     @width.setter
     def width(self, value):
-        if isinstance(value, int) is not True:
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
-        else:
-            self._width = value
+        
+        self.__width = value
 
     @property
     def height(self):
         """property that return the height instance modify
         by setter"""
-        return self._height
+        return self.__height
 
     @height.setter
     def height(self, value):
-        if isinstance(value, int) is False:
+        if not isinstance(value, int):
             raise TypeError("heigth must be an integer")
         if value < 0:
             raise ValueError("height must be >=0")
 
-        self._height = value
+        self.__height = value
