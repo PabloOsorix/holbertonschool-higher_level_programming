ad08cb2489571f67feacf69a0fb3c06933c4e0e7
PabloOsorix <3387@holbertonschool.com>
2022-01-24 20:47:03 -0500

    :heavy_plus_sign: Write class Rectangle with width, height, area and parameter
    it prints the area with # character

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+
+"""Module that contain a Rectangle class"""
+
+
+class Rectangle:
+    """Rectangle class that have width, height, area
+    perimeter and print the size of rectangle with #
+    character"""
+    def __init__(self, width=0, height=0):
+        self.height = height
+        self.width = width
+
+    @property
+    def width(self):
+        """property that return the width instance modify
+        by the setter"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if isinstance(value, int) is False:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """property that return the height instance modify
+        by setter"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if isinstance(value, int) is False:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    def perimeter(self):
+        if self.width == 0 or self.height == 0:
+            return 0
+        return 2 * (self.width + self.height)
+
+    def area(self):
+        return self.width * self.height
+
+    def __str__(self):
+        str_result = ""
+
+        for i in range(self.height):
+            str_result += "#" * self.width
+            if i != self.height - 1:
+                str_result += "\n"
+        return str_result
