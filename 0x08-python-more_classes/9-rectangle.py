bd1325b4565cc499f689b7a4e31c9d713be301ed
PabloOsorix <3387@holbertonschool.com>
2022-01-24 22:40:42 -0500

    :recycle: add square classmethod

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -81,3 +81,8 @@ class Rectangle:
         if rect_1.area() >= rect_2.area():
             return rect_1
         return rect_2
+
+    @classmethod
+    def square(cls, size=0):
+        """Class method that equals width and height with size"""
+        return cls(size, size)
