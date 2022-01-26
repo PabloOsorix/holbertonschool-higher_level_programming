3c7313971ba55e09132221c982bf7be2882adf68
PabloOsorix <3387@holbertonschool.com>
2022-01-25 21:09:34 -0500

    :heavy_minus_sign: remove test segment in the code

--- 0x09-python-everything_is_object/19-answer.py
+++ 0x09-python-everything_is_object/19-answer.py
@@ -7,16 +7,3 @@ it return a copy of a list"""
 def copy_list(l):
     copy_list = l[:]
     return copy_list
-
-
-
-my_list = [1, 2, 3]
-print(my_list)
-
-new_list = copy_list(my_list)
-
-print(my_list)
-print(new_list)
-
-print(new_list == my_list)
-print(new_list is my_list)
