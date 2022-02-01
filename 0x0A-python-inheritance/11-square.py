c36d65bb3a9532b89fe421a7ada9b02ddd580575
PabloOsorix <3387@holbertonschool.com>
2022-01-31 23:56:47 -0500

    :heavy_plus_sign: add 1-my_list

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,48 +1,6 @@
 #!/usr/bin/python3
 
-
-"""Module contains class BaseGeometry"""
-
-
-class BaseGeometry:
-    """class that raises an Exception with
-    the message area() is not implemented"""
-    def area(self):
-        """method that raise an exception"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """instance that evaluate value
-        name (string) used in the raise exception
-        value (int) variable to evaluate.
-        """
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-
-
-"""Module that contain class Rectangle"""
-
-
-class Rectangle(BaseGeometry):
-    """Class with inherits from class BaseGeometry"""
-    def __init__(self, width, height):
-        super().__init__()
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self.__width = width
-        self.__height = height
-
-    def area(self):
-        self.area = self.__width * self.__height
-        return self.area
-
-    def __str__(self):
-        str_result = ""
-        str_result = "[Rectangle] {}/{}".format(str(self.__width),
-                                                str(self.__height))
-        return str_result
+Square = __import__('10-square').Square
 
 
 """Module that contains Square class"""
