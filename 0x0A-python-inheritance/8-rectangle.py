28daac0edec5346ee928912ec846828ffb07fe8f
PabloOsorix <3387@holbertonschool.com>
2022-01-31 21:39:33 -0500

    :heavy_minus_sign: remove line 34

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -31,8 +31,23 @@ class Rectangle(BaseGeometry):
         width (int)
         height (int)
         """
-        super().__init__()
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
+
+
+r = Rectangle(3, 5)
+
+print(r)
+print(dir(r))
+
+try:
+    print("Rectangle: {} - {}".format(r.width, r.height))
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
+
+try:
+    r2 = Rectangle(4, True)
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
