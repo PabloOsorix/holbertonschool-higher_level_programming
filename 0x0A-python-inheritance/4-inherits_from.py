f45a7e2e53fdf79141db890aaa71233e7767ab86
PabloOsorix <3387@holbertonschool.com>
2022-01-31 19:13:59 -0500

    :heavy_minus_sign: remove test code

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,21 +4,13 @@
 
 
 def inherits_from(obj, a_class):
-    """Function that returns True if the object 
-    is an instance of a class that inherited 
+    """Function that returns True if the object
+    is an instance of a class that inherited
     (directly or indirectly) from the specified
     class ; otherwise False.
     obj (object) to check
     a_class(class) to check obj
     """
-    if type(a) != a_class:
+    if type(obj) != a_class:
         return True
     return False
-
-a = True
-if inherits_from(a, int):
-    print("{} inherited from class {}".format(a, int.__name__))
-if inherits_from(a, bool):
-    print("{} inherited from class {}".format(a, bool.__name__))
-if inherits_from(a, object):
-    print("{} inherited from class {}".format(a, object.__name__))
