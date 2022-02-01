6aa5e75ab28959614ca47deea73061338b001c9d
PabloOsorix <3387@holbertonschool.com>
2022-01-31 23:57:57 -0500

    :recycle: refactor my code

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,30 +1,14 @@
 #!/usr/bin/python3
 
+"""Class that defines Rectangle"""
 
-"""Module contains class BaseGeometry"""
-
-
-class BaseGeometry():
-    """New class with area method and integer validator"""
-
-    def area(self):
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-
-
-"""Module that contain class Rectangle"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """Class with inherits from class BaseGeometry"""
+    """Class that defines BaseGeometry"""
     def __init__(self, width, height):
-        super().__init__()
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
+        BaseGeometry.integer_validator(self, "width", width)
+        BaseGeometry.integer_validator(self, "height", height)
         self.__width = width
         self.__height = height
