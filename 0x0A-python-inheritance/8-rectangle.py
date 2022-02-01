c636b61eee5a7d5c2991e7860f86d99aa432782b
PabloOsorix <3387@holbertonschool.com>
2022-01-31 22:45:16 -0500

    :heavy_plus_sign: add same code

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -4,22 +4,18 @@
 """Module contains class BaseGeometry"""
 
 
-class BaseGeometry:
-    """class that raises an Exception with
-    the message area() is not implemented"""
+class BaseGeometry():
+    """New class with area method and integer validator"""
+
     def area(self):
-        """method that raise an exception"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """instance that evaluate value
-        name (string) used in the raise exception
-        value (int) variable to evaluate.
-        """
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+   
+         raise ValueError("{} must be greater than 0".format(name))
 
 
 """Module that contain class Rectangle"""
