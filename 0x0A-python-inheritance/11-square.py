e1de2f7900fb8868c2691ee2df30e4525e5ef7aa
PabloOsorix <3387@holbertonschool.com>
2022-01-31 23:10:34 -0500

    :heavy_plus_sign: writted  a class Square that inherits from Rectangle (9-rectangle.py). (task based on 10-square.py

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+
+
+"""Module contains class BaseGeometry"""
+
+
+class BaseGeometry:
+    """class that raises an Exception with
+    the message area() is not implemented"""
+    def area(self):
+        """method that raise an exception"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """instance that evaluate value
+        name (string) used in the raise exception
+        value (int) variable to evaluate.
+        """
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+"""Module that contain class Rectangle"""
+
+
+class Rectangle(BaseGeometry):
+    """Class with inherits from class BaseGeometry"""
+    def __init__(self, width, height):
+        super().__init__()
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        self.area = self.__width * self.__height
+        return self.area
+
+    def __str__(self):
+        str_result = ""
+        str_result = "[Rectangle] {}/{}".format(str(self.__width),
+                                                str(self.__height))
+        return str_result
+
+
+"""Module that contains Square class"""
+
+
+class Square(Rectangle):
+    """class with iinherits from Rectangle"""
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def __str__(self):
+        new_str = ""
+        new_str = "[Square] {}/{}".format(str(self.__size),
+                                          str(self.__size))
+        return new_str
