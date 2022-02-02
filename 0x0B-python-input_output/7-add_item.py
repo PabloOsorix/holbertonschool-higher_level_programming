318e87618ac7e586ce36ca0f672890641f149c7c
PabloOsorix <3387@holbertonschool.com>
2022-02-02 09:39:04 -0500

    :heavy_plus_sign: refactor code

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
+"""that adds all arguments to a Python list"""
 
-"""Add all arguments to a Python list and save them to a file."""
+
 import sys
 
-if __name__ == "__main__":
-    save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-    load_from_json_file = \
-        __import__('8-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-    try:
-        items = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        items = []
-    items.extend(sys.argv[1:])
-    save_to_json_file(items, "add_item.json")
+arglist = list(sys.argv[1:])
+
+try:
+    json_list = load_from_json_file('add_item.json')
+
+except Exception:
+    json_list = []
+
+json_list.extend(arglist)
+
+save_to_json_file(json_list, 'add_item.json')
