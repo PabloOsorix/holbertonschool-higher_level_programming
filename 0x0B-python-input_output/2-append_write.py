f1d4bf2fba60fc084b5c4140d1b73b2d42c70869
PabloOsorix <3387@holbertonschool.com>
2022-02-01 22:01:38 -0500

    :heavy_plus_sign: writted  a function that appends a string at the end of a text file (UTF8) and returns the number of characters added:

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""Module that contains the append_write dunction"""
+
+
+from isort import file
+
+
+def append_write(filename="", text=""):
+    with open(filename, mode="w+",  encoding="utf-8") as file:
+        file.seek(0, 2)
+        return(file.write(text))
