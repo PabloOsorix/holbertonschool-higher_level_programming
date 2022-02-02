351be2e9a5a3a74113b106d09db6e7985e305adf
PabloOsorix <3387@holbertonschool.com>
2022-02-02 00:01:44 -0500

    refactor

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,21 +1,16 @@
 #!/usr/bin/python3
-"""
-Script that adds all arguments to a
-Python list, and then save them to a file
-"""
 
-
+"""Add all arguments to a Python list and save them to a file."""
 import sys
-import os
-my_list = []
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 if __name__ == "__main__":
-    validator = os.path.exists("add_item.json")
-    if validator is True:
-        my_list = load_from_json_file("add_item.json")
-    for i in sys.argv[1:]:
-        my_list.append(i)
+    save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('8-load_from_json_file').load_from_json_file
 
-    save_to_json_file(my_list, "add_item.json")
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
