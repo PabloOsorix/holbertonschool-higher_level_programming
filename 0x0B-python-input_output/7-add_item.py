5367d1515725873da92f464d8dc7df83f5ab5298
PabloOsorix <3387@holbertonschool.com>
2022-02-01 23:47:42 -0500

    :heavy_plus_sign: spaces in line 5 adn 6

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,10 +2,10 @@
 """script that adds all
 arguments to a Python list,
 and then save them to a file:"""
+
+
 import sys
 import os
-
-
 my_list = []
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
