6890e1c92d189247c4d68d56047b5995ac3116a2
PabloOsorix <3387@holbertonschool.com>
2022-02-01 22:07:47 -0500

    :heavy_minus_sign: remove + in line 9

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -6,6 +6,6 @@
 def append_write(filename="", text=""):
     """Function that appends a string at the end of a text file
     (UTF8) and returns the number of characters added:"""
-    with open(filename, mode="w+",  encoding="utf-8") as file:
+    with open(filename, mode="w",  encoding="utf-8") as file:
         file.seek(0, 2)
         return(file.write(text))
