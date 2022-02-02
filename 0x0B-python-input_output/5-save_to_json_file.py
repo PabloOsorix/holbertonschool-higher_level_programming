8a0e20add91e13847ed0a43ca604c3af4e5977ee
PabloOsorix <3387@holbertonschool.com>
2022-02-01 23:08:22 -0500

    :recycle: change line 11 add 12

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -8,4 +8,5 @@ def save_to_json_file(my_obj, filename):
     """Function that writes an Object to
     a text file, using a JSON representation:"""
     with open(filename, mode="w", encoding="utf-8") as file:
-        json.dump(my_obj, file)
+        x = json.dumps(my_obj)
+        file.write(x)
