60ae403db6f829ee86a3da026493c7e4c79f9cb0
PabloOsorix <3387@holbertonschool.com>
2022-02-01 23:31:58 -0500

    :heavy_plus_sign: add return at line 11

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -8,4 +8,4 @@ def load_from_json_file(filename):
     """Function that creates an Object
     from a “JSON file”:"""
     with open(filename, encoding="utf-8") as file:
-        json.load(file)
+        return(json.load(file))
