f743c725a240a919f1f464299ce820e9c675696f
PabloOsorix <3387@holbertonschool.com>
2022-02-01 23:05:06 -0500

    :heavy_plus_sign: writeed a function that writes an Object to a text file, using a JSON representation:

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""Module that contains save_to_json_file"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """Function that writes an Object to
+    a text file, using a JSON representation:"""
+    with open(filename, mode="w", encoding="utf-8") as file:
+        x = json.dumps(my_obj)
+        file.write(x)
