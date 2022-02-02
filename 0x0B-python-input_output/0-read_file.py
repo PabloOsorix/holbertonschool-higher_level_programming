be5ac380e052b07742876bfb2a774d6e2fd949e8
PabloOsorix <3387@holbertonschool.com>
2022-02-01 21:17:03 -0500

    :heavy_plus_sign: writted a function that reads a text file (UTF8) and prints it to stdout:

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+"""This module contan function read_file"""
+
+
+def read_file(filename=""):
+    """function that reads a text file (UFT-8)
+    and prints it to stdout"""
+    with open(filename, encoding="utf-8") as my_file:
+        lines = my_file.read()
+        print(lines, end="")
+
+read_file("my_file_0.txt")
