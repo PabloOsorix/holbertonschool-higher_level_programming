a179a922ff61450a1921ca9b00dab165224a2ac4
PabloOsorix <3387@holbertonschool.com>
2022-02-02 09:42:45 -0500

    :recycle: refact code

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -6,53 +6,41 @@ prints the following statistics:
     - Total file size up to that point.
     - Count of read status codes up to that point.
 """
+import sys
 
 
-def print_stats(size, status_codes):
-    """Print accumulated metrics.
-    Args:
-        size (int): The accumulated read file size.
-        status_codes (dict): The accumulated count of status codes.
-    """
-    print("File size: {}".format(size))
-    for key in sorted(status_codes):
-        print("{}: {}".format(key, status_codes[key]))
-
-
-if __name__ == "__main__":
-    import sys
-
-    size = 0
-    status_codes = {}
-    valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
-    count = 0
-
-    try:
-        for line in sys.stdin:
-            if count == 10:
-                print_stats(size, status_codes)
-                count = 1
-            else:
-                count += 1
-
-            line = line.split()
-
+size = 0
+status_code = {"200": 0, "301": 0,
+               "400": 0, "401": 0,
+               "403": 0, "404": 0,
+               "405": 0, "500": 0}
+cont = 0
+try:
+    for line in sys.stdin:
+        splits = line.split()
+        if len(splits) > 1:
+            aux = cont
+            if splits[-2] in status_code:
+                status_code[splits[-2]] += 1
+                cont += 1
             try:
-                size += int(line[-1])
-            except (IndexError, ValueError):
-                pass
-
-            try:
-                if line[-2] in valid_codes:
-                    if status_codes.get(line[-2], -1) == -1:
-                        status_codes[line[-2]] = 1
-                    else:
-                        status_codes[line[-2]] += 1
-            except IndexError:
-                pass
-
-        print_stats(size, status_codes)
-
-    except KeyboardInterrupt:
-        print_stats(size, status_codes)
-        raise
+                size += int(splits[-1])
+                if aux == cont:
+                    cont += 1
+            except Exceptions:
+                if aux == cont:
+                    continue
+        if cont % 10 == 0:
+            print("File size: {:d}".format(size))
+            for key, value in sorted(status_code.items()):
+                if value:
+                    print("{:s}: {:d}".format(key, value))
+    print("File size: {:d}".format(size))
+    for key, value in sorted(status_code.items()):
+        if value:
+            print("{:s}: {:d}".format(key, value))
+except KeyboardInterrupt:
+    print("File size: {:d}".format(size))
+    for key, value in sorted(status_code.items()):
+        if value:
+            print("{:s}: {:d}".format(key, value))
