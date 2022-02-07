43dd8187c0aadbbad31ff61cf414d880f6ed6cad
Pablo Osorio <85521026+PabloOsorix@users.noreply.github.com>
2022-02-07 18:52:46 -0500

    Add description

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -6,6 +6,9 @@ from models.base import Base
 from models.rectangle import Rectangle
 import os
 
+"""Module to test square class"""
+
+
 class Test_RectangleClass(unittest.TestCase):
 
     def test_setter(self):
