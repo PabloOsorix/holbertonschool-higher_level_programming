8fa6d98d9f629399b3e85bf4bdd57e2fad5133d3
Pablo Osorio <85521026+PabloOsorix@users.noreply.github.com>
2022-03-15 23:43:36 -0500

    update comentary

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,4 +1,3 @@
-/*script that list the number
-of recrods with the same score*/
+-- Script that list the number of recrods with the same score.
 SELECT score, COUNT(*) AS number
 FROM second_table GROUP BY score;
