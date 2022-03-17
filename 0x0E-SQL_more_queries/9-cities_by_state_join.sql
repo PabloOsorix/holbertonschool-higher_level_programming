ee1d18181ca054f05f88811f35e30f5e5c3738ba
PabloOsorix <3387@holbertonschool.com>
2022-03-17 14:53:11 -0500

    :heavy_minus_sign: remove in line 3

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,4 @@
 -- Script that lists all cities contained in the database hbtn_0d_usa.
 SELECT cities.id, cities.name, states.name
-FROM hbtn_0d_usa.cities
-JOIN hbtn_0d_usa.states ON cities.state_id=states.id;
+FROM cities
+INNER JOIN hbtn_0d_usa.states ON cities.state_id = states.id;
