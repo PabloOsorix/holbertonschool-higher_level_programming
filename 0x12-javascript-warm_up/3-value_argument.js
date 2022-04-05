30002b91fe6117509060fc5bef42be7608ec3d01
PabloOsorix <3387@holbertonschool.com>
2022-04-04 20:21:12 -0500

    :heavy_minus_sign: remove for loop

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -5,8 +5,5 @@ const argv = process.argv;
 if (!argv[2]) {
   console.log('No argument');
 } else {
-  for (let i = 2; argv[i] != null; i++) {
-    console.log(argv[i]);
-    break;
-  }
+  console.log(argv[2]);
 }
