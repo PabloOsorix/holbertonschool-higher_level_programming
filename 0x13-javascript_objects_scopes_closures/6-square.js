ccb5a6e2c94cbeeb343cad5bbecef6924e72da18
PabloOsorix <3387@holbertonschool.com>
2022-04-07 09:50:41 -0500

    :heavy_plus_sign: add charPrint(c) to Square class

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+//  add method charPrint to Square class.
+const Rectangle = require('./4-rectangle');
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    this.print(c);
+  }
+}
+module.exports = Square;
