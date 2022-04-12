3e1f2e5ce638e864db10655924326c6f80c552a8
PabloOsorix <3387@holbertonschool.com>
2022-04-12 14:10:25 -0500

    :heavy_plus_sign: create script that computes the number of tasks completed by user id.

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+// script that computes the number of tasks completed by user id.
+
+const request = require('request');
+const url = process.argv[2]
+let counter = 0;
+let dict = {};
+request(url, (err, response, body) => {
+  if (err) console.log(err);
+  const content = JSON.parse(body);
+
+  for (let i = 0; content[i] != null; i++) {
+    if (!dict[content[i].userId]) {
+      if (content[i].completed == true) {
+        dict[content[i].userId] = 1;
+      } else {
+        dict[content[i].userId] = 0;
+      }
+    } else {
+      if (content[i].completed == false){
+        continue;
+      }
+      dict[content[i].userId] = dict[content[i].userId] + 1;
+    }
+  }
+  console.log(dict);
+});
