f797fef6801609f8c82017d1e282d35f237564dd
PabloOsorix <3387@holbertonschool.com>
2022-04-12 14:14:57 -0500

    :pencil2: fix semistandard

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -2,22 +2,21 @@
 // script that computes the number of tasks completed by user id.
 
 const request = require('request');
-const url = process.argv[2]
-let counter = 0;
-let dict = {};
+const url = process.argv[2];
+const dict = {};
 request(url, (err, response, body) => {
   if (err) console.log(err);
   const content = JSON.parse(body);
 
   for (let i = 0; content[i] != null; i++) {
     if (!dict[content[i].userId]) {
-      if (content[i].completed == true) {
+      if (content[i].completed === true) {
         dict[content[i].userId] = 1;
       } else {
         dict[content[i].userId] = 0;
       }
     } else {
-      if (content[i].completed == false){
+      if (content[i].completed === false) {
         continue;
       }
       dict[content[i].userId] = dict[content[i].userId] + 1;
