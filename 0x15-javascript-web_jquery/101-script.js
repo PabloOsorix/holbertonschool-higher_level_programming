c901e4085e4168af369b20bee7584fe7dfc383c6
PabloOsorix <3387@holbertonschool.com>
2022-04-13 22:40:00 -0500

    :heavy_plus_sign: create  a JavaScript script that adds, removes and clears LI elements from a list when the user clicks:

---
+++ 0x15-javascript-web_jquery/101-script.js
@@ -0,0 +1,12 @@
+const $ = window.$;
+$(document).ready(function () {
+  $('#add_item').on('click', function () {
+    $('.my_list').append('<li>Item</li>');
+  });
+  $('#remove_item').on('click', function () {
+    $('.my_list, li').last().remove();
+  });
+  $('#clear_list').on('click', function () {
+    $('.my_list').children('li').remove();
+  });
+});
