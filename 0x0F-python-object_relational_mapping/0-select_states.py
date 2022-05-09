41a01257fce83cbcd16790e1f5c9bd5ef10245ac
PabloOsorix <3387@holbertonschool.com>
2022-05-09 12:46:19 -0500

    :heavy_plus_sign:  script that lists all states from the database hbtn_0e_0_usa

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+#script that lists all states from the database hbtn_0e_0_usa
+import MySQLdb
+from sys import argv
+db = MySQLdb.connect(host="localhost", user=argv[1], port= 3306, passwd=argv[2], db=argv[3])
+cur = db.cursor()
+
+cur.execute("SELECT id, name FROM states ORDER BY states.id ASC")
+
+for states in cur.fetchall():
+        print (states)
+cur.close()
+db.close()
