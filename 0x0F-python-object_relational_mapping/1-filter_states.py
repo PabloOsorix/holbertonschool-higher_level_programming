2d343a773b835403dfa414bd8f23016dfb5d9fbc
PabloOsorix <3387@holbertonschool.com>
2022-05-09 15:34:51 -0500

    :memo: add permisses to execution

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -4,14 +4,14 @@
 import MySQLdb
 from sys import argv
 
-
-db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                     passwd=argv[2], db=argv[3])
-cursor = db.cursor()
-
-cursor.execute("SELECT id, name FROM states WHERE states.name LIKE 'N%' \
-               ORDER BY states.id")
-for rows in cursor.fetchall():
-    print(rows)
-cursor.close()
-db.close()
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
+    cursor = db.cursor()
+    
+    cursor.execute("SELECT id, name FROM states WHERE states.name LIKE 'N%' \
+                   ORDER BY states.id")
+    for rows in cursor.fetchall():
+        print(rows)
+    cursor.close()
+    db.close()
