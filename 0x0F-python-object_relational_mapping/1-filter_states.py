e718478068799d717f1687ac24638dbd50445f45
PabloOsorix <3387@holbertonschool.com>
2022-05-10 17:38:48 -0500

    :pencil2: add ASC in line e17

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     cursor = db.cursor()
 
     cursor.execute("SELECT id, name FROM states WHERE states.name LIKE 'N%' \
-                   ORDER BY states.id")
+                   ORDER BY states.id ASC")
     for rows in cursor.fetchall():
         print(rows)
     cursor.close()
