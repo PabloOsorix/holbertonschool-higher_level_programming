5a966bd9de776c0cdf86ad15869b8886d636ec30
PabloOsorix <3387@holbertonschool.com>
2022-05-10 10:44:03 -0500

    :pencil2: fix pydcode docs

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
-# script that takes in the name of a state as an
-# argument and lists all cities of that state,
-# using the database hbtn_0e_4_usa
+"""
+Script that takes in the name of a state as an
+argument and lists all cities of that state,
+using the database hbtn_0e_4_usa
+"""
 
 import MySQLdb
 from sys import argv
+
+
 if __name__ == "__main__":
 
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
