36bb95635aabfbc8922f6a95cb8331b4a111a295
PabloOsorix <3387@holbertonschool.com>
2022-05-10 10:29:54 -0500

    :pencil2: fix pycode line 4

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """
-script that lists all states from the database hbtn_0e_0_usa
-import MySQLdb
+script that lists all states from 
+the database hbtn_0e_0_usa
 """
 
+import MySQLdb
 from sys import argv
 
 
