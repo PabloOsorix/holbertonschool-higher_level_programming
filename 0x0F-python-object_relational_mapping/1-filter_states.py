3e4152f95eb7a6368c7a1d1315c237fa46ff8fa8
PabloOsorix <3387@holbertonschool.com>
2022-05-10 10:31:59 -0500

    :pencil2: fix pycode style

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
-# script that lists all states with a name
-# starting with N (upper N) from the database hbtn_0e_0_usa
+"""
+script that lists all states with a name
+starting with N (upper N) from the database
+hbtn_0e_0_usa
+"""
+
 import MySQLdb
 from sys import argv
 
@@ -8,7 +12,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3])
     cursor = db.cursor()
-    
+
     cursor.execute("SELECT id, name FROM states WHERE states.name LIKE 'N%' \
                    ORDER BY states.id")
     for rows in cursor.fetchall():
