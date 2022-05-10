ce079899364249670755f374ce099a0b799966fa
PabloOsorix <3387@holbertonschool.com>
2022-05-10 10:36:29 -0500

    :pencil2: fix pycode style

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-# script that takes in an argument and
-# displays all values in the states where
-# name matches the argument.
+"""
+script that takes in an argument and
+displays all values in the states where
+name matches the argument.
+"""
 
 import MySQLdb
 from sys import argv
