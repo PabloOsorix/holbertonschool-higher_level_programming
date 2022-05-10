8a2713340ce759e9c75f7a53baadce35785bb871
PabloOsorix <3387@holbertonschool.com>
2022-05-10 15:16:47 -0500

    :heavy_plus_sign: add

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -5,8 +5,8 @@ object from the database hbtn_0e_6_usa
 """
 
 import sqlalchemy
-from sys import argv
 from model_state import Base, State
+from sys import argv
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 
