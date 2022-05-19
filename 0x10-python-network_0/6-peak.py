edd443781b1a964433639174bc8298d4615e2afc
PabloOsorix <3387@holbertonschool.com>
2022-05-18 23:58:59 -0500

    :recycle: refact code

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,33 +1,14 @@
 #!/usr/bin/python3
-"""
-Module that contains function find_peak
-"""
+
+"""Function that finds a peak in a list of unsorted integers"""
 
 
 def find_peak(list_of_integers):
-    """
-    function that find the greather number
-    """
-    if not list_of_integers:
+    """ retuns a peak number from a unsorted list"""
+    if list_of_integers is None or len(list_of_integers) == 0:
         return None
-    tmp = 0
-    greather = 0
-    peak = 0
-    tmp2 = 1
-    tmp3 = 1
-    counter = 0
-    for index in list_of_integers:
-        counter += 1
-        if index == tmp and index != tmp2\
-                and tmp3 != tmp2 and counter != 2:
-            peak = index
-            return peak
-        if index >= greather:
-            greather = index
-        tmp = index + 1
-        tmp2 = tmp - 1
-        tmp3 = tmp2
-    return greather
+    list_of_integers.sort(reverse=True)
+    return list_of_integers[0]
 
 
 if __name__ == "__main__":
