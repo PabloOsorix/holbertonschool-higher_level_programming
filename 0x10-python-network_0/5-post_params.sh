06a5a87b3bc9c348d5096b2ab6cd7a711c3c9ac5
PabloOsorix <3387@holbertonschool.com>
2022-05-18 21:35:39 -0500

    :heavy_plus_sign: add

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that takes in a URL, nds a POST request and email/subject header variables
-curl -sX POST "$1" -H "email: test@gmail.com" -H "subject: I will always be here for PLD"
+curl -sd "email=test@gmail.com&subject=I will always be here for PLD" "$1"
