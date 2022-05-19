7a44d168b81734ac24a19fe566a1eb6e2e35e28e
PabloOsorix <3387@holbertonschool.com>
2022-05-18 21:15:14 -0500

    :heavy_plus_sign: add H flag

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #   Script that sends a GET request to the URL, and displays the body of the response
-curl -s $1"?X-HolbertonSchool-User-Id=98"
+curl -sH "?X-HolbertonSchool-User-Id=98" $1
