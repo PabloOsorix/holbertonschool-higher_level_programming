7f3467e5aecfc7859ca08c943976300700aa8dd8
PabloOsorix <3387@holbertonschool.com>
2022-05-18 23:55:15 -0500

    :heavy_plus_sign: fix pycode

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -13,23 +13,13 @@ def find_peak(list_of_integers):
     tmp = 0
     greather = 0
     peak = 0
-#    for index in range(0, len(list_of_integers), 2):
-#        if list_of_integers[index] == tmp:
-#            peak = list_of_integers[index]
-#            return peak
-#        tmp = list_of_integers[index] + 2
-#    if peak == 0:
-#        for index in list_of_integers:
-#            if index >= greather:
-#                greather = index
-#        return greather
     tmp2 = 1
     tmp3 = 1
     counter = 0
     for index in list_of_integers:
         counter += 1
-        if index == tmp and index != tmp2 \
-        and tmp3 != tmp2 and counter != 2:
+        if index == tmp and index != tmp2\
+                and tmp3 != tmp2 and counter != 2:
             peak = index
             return peak
         if index >= greather:
@@ -38,6 +28,7 @@ def find_peak(list_of_integers):
         tmp2 = tmp - 1
         tmp3 = tmp2
     return greather
+
 
 if __name__ == "__main__":
     find_peak()
