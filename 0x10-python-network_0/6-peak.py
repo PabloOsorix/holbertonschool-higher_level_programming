9dcb063da5b9e6ef93628dc5a30011a44f0a18d3
PabloOsorix <3387@holbertonschool.com>
2022-05-18 23:52:15 -0500

    :heavy_plus_sign: rewritte code ]

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -13,17 +13,31 @@ def find_peak(list_of_integers):
     tmp = 0
     greather = 0
     peak = 0
-    for i in range(0, len(list_of_integers), 2):
-        if list_of_integers[i] == tmp:
-            peak = list_of_integers[i]
+#    for index in range(0, len(list_of_integers), 2):
+#        if list_of_integers[index] == tmp:
+#            peak = list_of_integers[index]
+#            return peak
+#        tmp = list_of_integers[index] + 2
+#    if peak == 0:
+#        for index in list_of_integers:
+#            if index >= greather:
+#                greather = index
+#        return greather
+    tmp2 = 1
+    tmp3 = 1
+    counter = 0
+    for index in list_of_integers:
+        counter += 1
+        if index == tmp and index != tmp2 \
+        and tmp3 != tmp2 and counter != 2:
+            peak = index
             return peak
-        tmp = list_of_integers[i] + 2
-    if peak == 0:
-        for i in list_of_integers:
-            if i >= greather:
-                greather = i
-        return greather
-
+        if index >= greather:
+            greather = index
+        tmp = index + 1
+        tmp2 = tmp - 1
+        tmp3 = tmp2
+    return greather
 
 if __name__ == "__main__":
     find_peak()
