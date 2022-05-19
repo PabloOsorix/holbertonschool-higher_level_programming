7fbc063c636f19dd51ade57583d6a696d315a1f8
PabloOsorix <3387@holbertonschool.com>
2022-05-18 22:58:12 -0500

    :heavy_plus_sign: add script that find a peak

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Module that contains function find_peak
+"""
+
+
+def find_peak(list_of_integers):
+    """
+    function that find the greather number
+    """
+    if not list_of_integers:
+        return None
+    tmp = 0
+    greather = 0
+    peak = 0
+    for i in range(0, len(list_of_integers), 2):
+        if list_of_integers[i] == tmp:
+            peak = list_of_integers[i]
+            return peak
+        tmp = list_of_integers[i] + 2
+    if peak == 0:
+        for i in list_of_integers:
+            if i >= greather:
+                greather = i
+        return greather
+
+if __name__ == "__main__":
+    find_peak()
