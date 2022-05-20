c6317ab759d88d0b5b677a19d3e06ae7aaf274a1
PabloOsorix <3387@holbertonschool.com>
2022-05-20 16:29:07 -0500

    :heavy_plus_sign: add decoded in utf-8 line 16

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -13,6 +13,6 @@ if __name__ == "__main__":
     url = sys.argv[1]
     try:
         with urllib.request.urlopen(url) as response:
-            print(response.read())
+            print(response.read().decode('utf-8'))
     except urllib.error.HTTPError as error:
         print("Error code: {}".format(error.getcode()))
