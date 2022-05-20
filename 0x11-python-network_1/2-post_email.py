607b5ba68e002df77376efabe09ca45d55cd1b03
PabloOsorix <3387@holbertonschool.com>
2022-05-20 14:48:15 -0500

    :heavy_plus_sign: script that send a variable email in a POST request

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Script that send a variable email
+in a POST request.
+"""
+import sys
+import urllib.request
+
+
+if __name__ == "__main__":
+    email = sys.argv[2]
+    data = urllib.parse.urlenconde(email)
+    data = data.encode('utf-8')
+    with urllib.request.urlopen(sys.argv[1], data) as response:
+        response = response.read().decode('utf-8')
+
+    print(response)
