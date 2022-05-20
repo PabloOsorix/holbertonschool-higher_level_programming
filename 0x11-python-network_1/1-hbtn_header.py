9cf87ddd897c66b39b355564ffec721245bd57a0
PabloOsorix <3387@holbertonschool.com>
2022-05-20 14:03:34 -0500

    :heavy_plus_sign: add if __name__ conditional in line 9

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -6,7 +6,7 @@ X-Request from a response
 import sys
 import urllib.request
 
-
-with urllib.request.urlopen(sys.argv[1]) as response:
-    header = response.headers
-    print(header.get('X-Request-Id'))
+if __name__ == "__main__":
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        header = response.headers
+        print(header['X-Request-Id'])
