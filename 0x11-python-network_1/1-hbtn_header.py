85e8d101c4e4bcf1006d87dd3f252990540858b1
PabloOsorix <3387@holbertonschool.com>
2022-05-20 14:05:15 -0500

    :rewind: come back to .get()

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -9,4 +9,4 @@ import urllib.request
 if __name__ == "__main__":
     with urllib.request.urlopen(sys.argv[1]) as response:
         header = response.headers
-        print(header['X-Request-Id'])
+        print(header.get('X-Request-Id'))
